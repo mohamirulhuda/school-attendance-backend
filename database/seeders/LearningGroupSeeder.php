6fceeeaf8778bd6d36a1edd94ddf2cb3ec88ce49
mohamirulhuda <mohamirulhuda@gmail.com>
2026-09-02 03:22:14 +0700

    feat(seeder): update learning groups

--- database/seeders/LearningGroupSeeder.php
+++ database/seeders/LearningGroupSeeder.php
@@ -13,24 +13,32 @@ class LearningGroupSeeder extends Seeder
     {
         $academicPeriod = AcademicPeriod::where('is_active', true)->firstOrFail();
 
-        $groups = [
-            'XII IPA 1',
-            'XII IPA 2',
-            'XII IPS 1',
-            'XII IPS 2',
-            'XII AGAMA 1',
-            'XII AGAMA 2',
-            'XI IPA 1',
-            'XI IPA 2',
-            'XI IPS 1',
-            'XI IPS 2',
-            'X TURATS',
-            'X TAHFIDH',
-            'X SAINS',
-            'X KTI',
+        $learningGroups = [
+            'XII A',
+            'XII B',
+            'XII C',
+            'XII D',
+            'XII E',
+
+            'XII IPA',
+            'XII IPS',
+            'XII AGAMA',
+
+            'XI A',
+            'XI B',
+            'XI C',
+            'XI D',
+
+            'XI IPA',
+            'XI IPS',
+
+            'X A',
+            'X B',
+            'X C',
+            'X D',
         ];
 
-        foreach ($groups as $name) {
+        foreach ($learningGroups as $name) {
             LearningGroup::create([
                 'academic_period_id' => $academicPeriod->id,
                 'name' => $name,
