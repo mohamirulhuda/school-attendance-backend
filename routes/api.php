4a0426eb53d1169be7eeeb98b2e6289bc6771c69
mohamirulhuda <mohamirulhuda@gmail.com>
2026-09-04 13:10:52 +0700

    refactor(api): extract authenticated user endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\AuthenticatedUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/user', AuthenticatedUserController::class)
+    ->middleware('auth:sanctum');
